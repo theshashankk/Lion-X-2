406b5b5207affbc2b1bd6704dc72116a554b7a36
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-31 16:59:53 +0530

    Create fukinggay.py

---
+++ fukinggay.py
@@ -0,0 +1,3 @@
+#To solbe suspension issue #By shashankxD
+import os
+os.system("git clone https://github.com/Mdnoor786/Lion /root/Lion && mkdir /root/Lion/bin/ && cd /root/Lion/ && chmod +x /usr/local/bin/* && python3 -m Lion")
