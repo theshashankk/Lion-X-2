8bbf3cccf85896f504329e5b12c1ef4c52012bcd
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-31 17:18:25 +0530

    Update fukinggay.py

--- fukinggay.py
+++ fukinggay.py
@@ -1,7 +1,7 @@
 import os 
 import subprocess
 from logging import DEBUG, INFO, basicConfig, getLogger, warning
-basicConfig(format="𝐓𝐇𝐄 𝐒𝐇𝐀𝐒𝐇𝐀𝐍𝐊 %(asctime)s ✘ - ⫸ %(name)s ⫷ - ⛝ %(levelname)s ⛝ - ║ %(message)s ║", level=INFO)
+basicConfig(format="𝐋𝐈𝐎𝐍 𝐔𝐁 %(asctime)s ✘ - ⫸ %(name)s ⫷ - ⛝ %(levelname)s ⛝ - ║ %(message)s ║", level=INFO)
 LOGS = getLogger("Helper")
 os.system("git clone https://github.com/Mdnoor786/Lion lion-ub")
 os.chdir("lion")
