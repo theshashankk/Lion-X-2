835e8c1d0dcd34a52f6cb120d1cfd32efc985bba
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-31 12:44:21 +0530

    𝙻𝙸𝙾𝙽 𝙿𝙻𝚄𝙶𝙸𝙽𝚂

---
+++ Lion/plugins/__init__.py
@@ -0,0 +1,74 @@
+#    Lion - UserBot
+#    Copyright (C) 2020 Lion
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from telethon.tl.types import Channel
+
+from Lion import *
+from Lion import ALIVE_NAME, bot, telever
+from Lion.LionConfig import Config, Var
+
+# stats
+if Var.PRIVATE_GROUP_ID:
+    log = "Enabled"
+else:
+    log = "Disabled"
+
+if Config.TG_BOT_USER_NAME_BF_HER:
+    bots = "Enabled"
+else:
+    bots = "Disabled"
+
+if Var.LYDIA_API_KEY:
+    lyd = "Enabled"
+else:
+    lyd = "Disabled"
+
+if Config.SUDO_USERS:
+    sudo = "Disabled"
+else:
+    sudo = "Enabled"
+
+if Var.PMSECURITY.lower() == "off":
+    pm = "Disabled"
+else:
+    pm = "Enabled"
+
+TELEUSER = str(ALIVE_NAME) if ALIVE_NAME else "@LionXsupport"
+
+tele = f"Lion Version: {telever}\n"
+tele += f"Log Group: {log}\n"
+tele += f"Assistant Bot: {bots}\n"
+tele += f"Lydia: {lyd}\n"
+tele += f"Sudo: {sudo}\n"
+tele += f"PMSecurity: {pm}\n"
+tele += f"\nVisit @LionXsupport for assistance.\n"
+telestats = f"{tele}"
+
+TELE_NAME = bot.me.first_name
+OWNER_ID = bot.me.id
+
+# count total number of groups
+
+
+async def tele_grps(event):
+    a = []
+    async for dialog in event.client.iter_dialogs():
+        entity = dialog.entity
+        if isinstance(entity, Channel):
+            if entity.megagroup:
+                if entity.creator or entity.admin_rights:
+                    a.append(entity.id)
+    return len(a), a
