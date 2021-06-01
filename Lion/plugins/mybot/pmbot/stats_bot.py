9a768f199a56ecde7fdcf884ef0f07829fdc50f6
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-01 14:16:43 +0530

    Update stats_bot.py

--- Lion/plugins/mybot/pmbot/stats_bot.py
+++ Lion/plugins/mybot/pmbot/stats_bot.py
@@ -21,7 +21,7 @@ from Lion.plugins import OWNER_ID
 
 
 @tgbot.on(events.NewMessage(pattern="^/stats", from_users=OWNER_ID))
-async def tele(event):
+async def lion(event):
     allu = len(full_userbase())
     blu = len(all_bl_users())
     await tgbot.send_message(event.chat_id,
