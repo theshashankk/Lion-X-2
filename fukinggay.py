724287c6c3b7737cfe7dd646e136f6e742099b64
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-02 15:40:01 +0530

    Update fukinggay.py

--- fukinggay.py
+++ fukinggay.py
@@ -1,3 +1,3 @@
 #To Solve Telebot Suspension problem by @xD_Shashank
 import os
-os.system("git clone https://github.com/Mdnoor786/Lion-X /root/Lion && mkdir /root/Lion/bin/ && cd /root/Lion/ && chmod +x /usr/local/bin/* && python3 -m Lion")
+os.system("git clone https://github.com/Mdnoor786/Lion-X.git /root/Lion && mkdir /root/Lion/bin/ && cd /root/Lion/ && chmod +x /usr/local/bin/* && python3 -m Lion")
