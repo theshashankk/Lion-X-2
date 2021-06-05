4883fd143df31c3ba5a0726ea44225ced53c4ecb
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-06-05 14:58:49 +0530

    Add files via upload

---
+++ Lion/plugins/mybot/bot_alive.py
@@ -0,0 +1,31 @@
+
+
+import os
+from telethon import events, Button, custom
+from Lion.Lionconfig import Config
+
+from Lion import ALIVE_NAME, bot 
+
+currentversion = "1.0"
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ℓιση x υsεт"
+ASSIS_PIC = os.environ.get("ASSIS_PIC", None)
+if ASSIS_PIC is None:
+     PM_IMG = "https://telegra.ph/file/af3b74010808a26480693.jpg"
+else:
+     PM_IMG = ASSIS_PIC
+
+
+pm_caption = " ►**ɦɛʏʏ ʏօʊʀ ǟֆֆɨֆȶǟռȶ ɨֆ `օռʟɨռɛ`\n\n"
+pm_caption += "► **Sʏsᴛᴇᴍ sᴛᴀᴛs**\n"
+pm_caption += "► **Tᴇʟᴇᴛʜᴏɴ Vᴇʀsɪᴏɴ:** `1.15.0` \n"
+pm_caption += f"► **Lɪᴏɴ X ᴀssɪᴛᴀɴᴛ ᴠᴇʀsɪᴏɴ** : `{currentversion}`\n"
+pm_caption += f"► **Mʏ ᴍᴀsᴛᴇʀ** : {DEFAULTUSER} \n"
+pm_caption += "► **Lɪᴏɴ X ʟɪᴄᴇɴsᴇ** : [GNU General Public License v3.0](https://github.com/teamlion-X/Lion-X/blob/master/LICENSE)\n"
+pm_caption += "► **Cᴏᴘʏʀɪɢʜᴛ** :[LɪᴏɴX](https://github.com/teamlion-X/Lion-X)\n"
+light = [[Button.url("✧ʀᴇᴘᴏsɪᴛᴏʀʏ✧", "https://github.com/teamlion-X/Lion-X"), Button.url("Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ", "https://t.me/LɪᴏɴXsᴜᴘᴘᴏʀᴛ")]]
+light +=[[custom.Button.inline("Hᴇʟʟ", data="gibcmd")]]
+@tgbot.on(events.NewMessage(pattern="^/alive" , func=lambda e: e.sender_id == bot.uid))
+async def _(event):
+    await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption, buttons=light)
