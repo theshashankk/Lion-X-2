d9a6ff0f8da64c4eac1a82b1d572282d04bb0e81
TeamlionUB <58950863+TeamLion-X@users.noreply.github.com>
2021-06-06 10:31:16 +0000

    pylint: auto fixes

--- Lion/plugins/alive.py
+++ Lion/plugins/alive.py
@@ -4,9 +4,8 @@ import time
 from telethon import version
 from userbot.utils import admin_cmd, sudo_cmd
 
-from Lion.LionConfig import Config, Var
-
 from Lion import StartTime
+from Lion.LionConfig import Var
 
 # ======CONSTANTS=========#
 CUSTOM_ALIVE = Var.CUSTOM_ALIVE if Var.CUSTOM_ALIVE else "ʏօօ!! ʟɨօռ ʊֆɛʀɮօȶ ɨֆ ǟʟɨʋɛ!"
