748d6592ff91b569a8a266ec70ae87e2e840b4a8
TeamlionUB <TeamLion-X@users.noreply.github.com>
2021-06-06 11:35:08 +0000

    pylint: auto fixes

--- Lion/helper/functions.py
+++ Lion/helper/functions.py
@@ -1,11 +1,6 @@
-import requests , os, re, time
+import requests
+import os
 from bs4 import BeautifulSoup
-from asyncio import sleep
-from random import choice
-from telethon import events
-from PIL import Image
-from validators.url import url
-from telethon.tl.types import Channel
 
 
 async def get_readable_time(seconds: int) -> str:
@@ -15,7 +10,9 @@ async def get_readable_time(seconds: int) -> str:
     time_suffix_list = ["s", "m", "h", "days"]
     while count < 4:
         count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        remainder, result = divmod(
+            seconds, 60) if count < 3 else divmod(
+            seconds, 24)
         if seconds == 0 and remainder == 0:
             break
         time_list.append(int(result))
@@ -30,31 +27,43 @@ async def get_readable_time(seconds: int) -> str:
     return up_time
 
 
-async def darkmusic(dark , QUALITY):
-  search = dark
-  headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
-  html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
-  soup = BeautifulSoup(html, 'html.parser')
-  for link in soup.find_all('a'):
-    if '/watch?v=' in link.get('href'):
-        # May change when Youtube Website may get updated in the future.
-        video_link = link.get('href') 
-        break
-  video_link =  'http://www.youtube.com/'+video_link
-  command = ('youtube-dl --extract-audio --audio-format mp3 --audio-quality ' + QUALITY + ' ' + video_link)	
-  os.system(command)
-
-
-async def darkmusicvideo(dark):
+async def darkmusic(dark, QUALITY):
     search = dark
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
-    html = requests.get('https://www.youtube.com/results?search_query='+search, headers=headers).text
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+    html = requests.get(
+        'https://www.youtube.com/results?search_query=' +
+        search,
+        headers=headers).text
     soup = BeautifulSoup(html, 'html.parser')
     for link in soup.find_all('a'):
         if '/watch?v=' in link.get('href'):
             # May change when Youtube Website may get updated in the future.
-            video_link = link.get('href') 
-            break    
-    video_link =  'http://www.youtube.com/'+video_link
-    command = ('youtube-dl -f "[filesize<20M]" ' +video_link)  
+            video_link = link.get('href')
+            break
+    video_link = 'http://www.youtube.com/' + video_link
+    command = (
+        'youtube-dl --extract-audio --audio-format mp3 --audio-quality ' +
+        QUALITY +
+        ' ' +
+        video_link)
+    os.system(command)
+
+
+async def darkmusicvideo(dark):
+    search = dark
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+    html = requests.get(
+        'https://www.youtube.com/results?search_query=' +
+        search,
+        headers=headers).text
+    soup = BeautifulSoup(html, 'html.parser')
+    for link in soup.find_all('a'):
+        if '/watch?v=' in link.get('href'):
+            # May change when Youtube Website may get updated in the future.
+            video_link = link.get('href')
+            break
+    video_link = 'http://www.youtube.com/' + video_link
+    command = ('youtube-dl -f "[filesize<20M]" ' + video_link)
     os.system(command)
